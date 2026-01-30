f8459810e48084b019042b0bc038f389b7691e3e
patrickmokre <42693105+patrickmokre@users.noreply.github.com>
2026-01-30 14:01:53 +0100

    Added BW Version of Plot

--- 2026-04/tt2604-00-plot.R
+++ 2026-04/tt2604-00-plot.R
@@ -101,3 +101,27 @@ y_bottom <- bbox$ymin + 0.45 * (bbox$ymax - bbox$ymin)   # 45th percentile
   ) %>% 
   ggsave("tt2604-d1.png", plot=., width=5, height=5, units="cm")
 
+(ggplot() +
+    geom_sf(data = hex_tt %>% filter(share=="Rest"), aes(fill = share), color = "black", linewidth = .4) +
+    geom_sf(data = hex_tt %>% filter(share=="Top 25 Firms"), aes(fill = share), color = "white", linewidth = .4) +
+    # geom_sf(data = brazil, fill = NA, color = "black", linewidth = 0.5) + # optional outline
+    annotate("label", x = x_center - 4e+05, y = y_top,
+             label = paste0(" ", nfirms_top50, " \n Firms "), size = 6, lineheight = .3,
+             fill = "white", color = "#000000", fontface = "bold", family="Londrina") +
+    annotate("label", x = x_center + 7e+05, y = y_bottom,
+             label = paste0(" ", nfirms_rest, " \n Firms "), size = 6, lineheight = .3,
+             fill = "white", color = "#000000", fontface = "bold", family="Londrina") +
+    scale_fill_manual(values = c("Top 25 Firms" = "#000000", "Rest" = "#ffffff")) +
+    labs(
+      title = paste0(" ", nfirms_top50, " Firms own half the \n capital stock in Brazil ") %>% toupper(),
+      caption = "Data: Tidy Tuesday 2026, Week 4"
+    ) +
+    theme_void(base_family = "Londrina") +
+    theme(
+      legend.position = "none",
+      plot.title = element_text(size = 32, hjust = 0.5, lineheight = .3, color="#000000"),
+      plot.caption = element_text(size=20, hjust = 0.5, color="#000000")
+    )
+) %>% 
+  ggsave("tt2604-d2-bw.png", plot=., width=5, height=5, units="cm")
+
